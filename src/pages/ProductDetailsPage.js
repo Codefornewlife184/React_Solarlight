6d7994c70c5d1795ba87d5a3fb9ac5a249c67dd9
Code For New Life <codefornewlife@gmail.com>
2024-10-13 00:37:05 +0300

    F01

---
+++ src/pages/ProductDetailsPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Spacer2 from "../Components/common/Spacer2";
+import ProductDetails from "../Components/product/ProductDetails";
+
+function ProductDetailsPage() {
+  return (
+    <>
+      <Spacer2 />
+      <ProductDetails />
+    </>
+  );
+}
+
+export default ProductDetailsPage;
